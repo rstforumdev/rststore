ae1bb20be14a46318ce11cff1b6ddb8b42ca0b75
Rahul Sharma <rahulat4200@gmail.com>
2021-02-12 08:31:38 +0530

    21. Basic Redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
 import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 )
