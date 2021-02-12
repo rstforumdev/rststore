ea7a3c2c6209e9dd90459086c8e02b49d0f7e8e9
Rahul Sharma <rahulat4200@gmail.com>
2021-02-12 09:55:17 +0530

    22. Created the first reducer, the productListReducer. Imported to the store as well. This is part 1, but next we have to put all our actions as constants and complete the actions/reducers part and load data into our productList state.

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,17 @@
+// Handle state for the product list
+// reducers takes two args, initial state and an action
+// so we will DISPATCH ACTIONS to this reducer later on
+// action is an object that has a type and an optional payload
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    // product list request
+    case 'PRODUCT_LIST_REQUEST':
+      return { loading: true, products: [] }
+    case 'PRODUCT_LIST_SUCCESS':
+      return { loading: false, products: action.payload }
+    case 'PRODUCT_LIST_FAIL':
+      return { loading: false, error: action.payload }
+    default:
+      return state // return the initial state
+  }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers.js'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList: productListReducer // productList will be a state variable
+  // add the above first and then see this state in the devtools
+})
 
 const initialState = {}
 
