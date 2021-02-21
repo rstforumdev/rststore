236b0526707ddcda9f36cf20c8c85e54682246ac
Rahul Sharma <rahulat4200@gmail.com>
2021-02-22 05:01:28 +0530

    34. Finished implemented User Registration with Password Encryption. Next integration this functionality with React and Redux.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,21 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await brcrypt.compare(enteredPassword, this.password)
 }
 
+// `pre` method with value `save` will run the code before saving
+userSchema.pre('save', async function (next) {
+  // do this only if the password field is sent or modified
+  // later on when we make an updateProfile function
+  // we don't want this function to run or else we'll
+  // have a new salt generated and we won't be able to login
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  // Salt is request to hash the password asynchronously
+  const salt = await brcrypt.genSalt(10)
+  this.password = await brcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
