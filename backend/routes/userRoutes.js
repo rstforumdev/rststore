fadcf14bad4146c2ec4e61dd908327c158c0e9cb
Rahul Sharma <rahulat4200@gmail.com>
2021-02-23 08:11:19 +0530

    40. Updated User Profile endpoint in the backend, with new controller.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router()
 import {
   authUser,
   registerUser,
-  getUserProfile
+  getUserProfile,
+  updateUserProfile
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -11,6 +12,9 @@ router.route('/').post(registerUser)
 router.post('/login', authUser)
 // `/profile` page should be protected
 // just add it before the controller function
-router.route('/profile').get(protect, getUserProfile)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
