245f1e18b0d8577c8e76f42556bebfb81781df5d
Rahul Sharma <rahulat4200@gmail.com>
2021-03-08 05:37:14 +0530

    Implemented Product Edit Screen

---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,166 @@
+// Copied from the Register Screen
+import React, { useState, useEffect } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import {
+  Button,
+  Flex,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Spacer,
+  Link,
+  Checkbox
+} from '@chakra-ui/react'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { listProductDetails } from '../actions/productActions' // STEP 68
+
+const ProductEditScreen = ({ match, history }) => {
+  const productId = match.params.id
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState(0)
+  const [image, setImage] = useState('')
+  const [brand, setBrand] = useState('')
+  const [countInStock, setCountInStock] = useState(0)
+  const [category, setCategory] = useState('')
+  const [description, setDescription] = useState('')
+
+  const dispatch = useDispatch()
+
+  const productDetails = useSelector(state => state.productDetails)
+  const { loading, error, product } = productDetails
+
+  useEffect(() => {
+    if (!product.name || product._id !== productId) {
+      dispatch(listProductDetails(productId))
+    } else {
+      setName(product.name)
+      setPrice(product.price)
+      setImage(product.image)
+      setBrand(product.brand)
+      setCategory(product.category)
+      setCountInStock(product.countInStock)
+      setDescription(product.description)
+    }
+  }, [dispatch, history, productId, product])
+
+  const submitHandler = e => {
+    e.preventDefault()
+    // STEP 63 => UPDATE PRODUCT
+  }
+
+  return (
+    <>
+      <Link as={RouterLink} to='/admin/productlist'>
+        Go Back
+      </Link>
+      <Flex w='full' alignItems='center' justifyContent='center' py='5'>
+        <FormContainer>
+          <Heading as='h1' mb='8' fontSize='3xl'>
+            Edit Product
+          </Heading>
+          {/* STEP 63 -> checking for loadingUpdate and errorUpdate */}
+          {/* {loadingUpdate && <Loader />}
+          {errorUpdate && <Message type='error'>{errorUpdate}</Message>} */}
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <Message type='error'>{error}</Message>
+          ) : (
+            <form onSubmit={submitHandler}>
+              {/* NAME */}
+              <FormControl id='name' isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  type='text'
+                  placeholder='Enter Full Name'
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                />
+              </FormControl>
+              <Spacer h='3' />
+              {/* PRICE */}
+              <FormControl id='email' isRequired>
+                <FormLabel>Price</FormLabel>
+                <Input
+                  type='number'
+                  placeholder='Enter price'
+                  value={price}
+                  onChange={e => setPrice(e.target.value)}
+                />
+              </FormControl>
+              <Spacer h='3' />
+              {/* IMAGE */}
+              <FormControl id='image' isRequired>
+                <FormLabel>Image</FormLabel>
+                <Input
+                  type='text'
+                  placeholder='Enter image url'
+                  value={image}
+                  onChange={e => setImage(e.target.value)}
+                />
+              </FormControl>
+              <Spacer h='3' />
+              {/* DESCRIPTION */}
+              <FormControl id='text' isRequired>
+                <FormLabel>Description</FormLabel>
+                <Input
+                  type='text'
+                  placeholder='Enter description'
+                  value={description}
+                  onChange={e => setDescription(e.target.value)}
+                />
+              </FormControl>
+              <Spacer h='3' />
+              {/* BRAND */}
+              <FormControl id='brand' isRequired>
+                <FormLabel>Brand</FormLabel>
+                <Input
+                  type='text'
+                  placeholder='Enter brand'
+                  value={brand}
+                  onChange={e => setBrand(e.target.value)}
+                />
+              </FormControl>
+              <Spacer h='3' />
+              {/* COUNT IN STOCK */}
+              <FormControl id='countInStock' isRequired>
+                <FormLabel>Count in Stock</FormLabel>
+                <Input
+                  type='number'
+                  placeholder='Enter count'
+                  value={countInStock}
+                  onChange={e => setCountInStock(e.target.value)}
+                />
+              </FormControl>
+              <Spacer h='3' />
+              {/* CATEGORY */}
+              <FormControl id='brand' isRequired>
+                <FormLabel>Category</FormLabel>
+                <Input
+                  type='text'
+                  value={category}
+                  placeholder='Enter category'
+                  onChange={e => setCategory(e.target.value)}
+                />
+              </FormControl>
+              <Button
+                isLoading={loading}
+                type='submit'
+                mt='4'
+                colorScheme='teal'>
+                Update
+              </Button>
+            </form>
+          )}
+        </FormContainer>
+      </Flex>
+    </>
+  )
+}
+
+export default ProductEditScreen
